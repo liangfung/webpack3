c420b98e01ef279350dfad80205f6387c46cfdb2
liangfung <1098486429@qq.com>
2017-12-23 18:05:03 +0800

    entry & output

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+module.exports = {
+  entry: {
+    entry: './src/entry.js',
+    entry2: './src/entry2.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  }
+}
