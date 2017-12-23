b3ca5055a1ff4ab448d247398caab0fb74496ad0
liangfung <1098486429@qq.com>
2017-12-23 18:25:35 +0800

    服务和热更新

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,13 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
+  },
+  module: {},
+  plugins: [],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    host: '172.17.5.17',
+    compress: true,
+    port: 8787
   }
 }
